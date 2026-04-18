464277079a3c9a209e4c74a18988fd0cbe077ad2
YueXia-DeLiSha <750243132@qq.com>
2026-04-18 16:15:29 +0800

    站点更新: YYYY-04-2026年4月18日 16:15:08

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -1,67 +1,62 @@
-// autoload.js - 德丽莎本地模型配置
-// 模型文件路径：/live2d_models/delisha/delisha.model.json
+// live2d_path 参数建议使用绝对路径
+// const live2d_path = "https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/";
 const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
-    return new Promise((resolve, reject) => {
-        let tag;
+	return new Promise((resolve, reject) => {
+		let tag;
 
-        if (type === "css") {
-            tag = document.createElement("link");
-            tag.rel = "stylesheet";
-            tag.href = url;
-        } else if (type === "js") {
-            tag = document.createElement("script");
-            tag.src = url;
-        }
-        if (tag) {
-            tag.onload = () => resolve(url);
-            tag.onerror = () => reject(url);
-            document.head.appendChild(tag);
-        }
-    });
+		if (type === "css") {
+			tag = document.createElement("link");
+			tag.rel = "stylesheet";
+			tag.href = url;
+		}
+		else if (type === "js") {
+			tag = document.createElement("script");
+			tag.src = url;
+		}
+		if (tag) {
+			tag.onload = () => resolve(url);
+			tag.onerror = () => reject(url);
+			document.head.appendChild(tag);
+		}
+	});
 }
 
-// 加载核心资源并初始化
+// 加载 waifu.css live2d.min.js waifu-tips.js
 if (screen.width >= 768) {
-    Promise.all([
-        loadExternalResource(live2d_path + "waifu.css", "css"),
-        loadExternalResource(live2d_path + "live2d.min.js", "js"),
-        loadExternalResource(live2d_path + "waifu-tips.js", "js")
-    ]).then(() => {
-        // 直接调用 L2Dwidget.init 加载本地模型
-        L2Dwidget.init({
-            model: {
-                jsonPath: "/live2d_models/delisha/delisha.model.json"
-            },
-            display: {
-                position: "right",
-                width: 150,
-                height: 300,
-                hOffset: 0,
-                vOffset: -20
-            },
-            mobile: {
-                show: true,
-                scale: 0.5
-            },
-            react: {
-                opacityDefault: 0.7,
-                opacityOnHover: 0.2
-            },
-            dialog: {
-                enable: true,
-                script: {
-                    'tap body': '呀，舰长，你碰到我了！TeriTeri~',
-                    'tap face': '呜…不要摸我的脸啦！',
-                    'tap head': '嘿嘿，舰长，有什么事吗？',
-                    'every idle 20s': '今天也要加油哦，舰长！'
-                }
-            }
-        });
-        console.log("✅ 德丽莎 (本地模型) 加载成功！");
-    }).catch(err => {
-        console.error("❌ 德丽莎资源加载失败：", err);
-    });
+	Promise.all([
+		loadExternalResource(live2d_path + "waifu.css", "css"),
+		loadExternalResource(live2d_path + "live2d.min.js", "js"),
+		loadExternalResource(live2d_path + "waifu-tips.js", "js")
+	]).then(() => {
+		// 配置选项的具体用法见 README.md
+		initWidget({
+			waifuPath: live2d_path + "waifu-tips.json",
+			//apiPath: "https://live2d.fghrsh.net/api/",
+			cdnPath: "/live2d_models/",
+			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
+		});
+	});
 }
+
+console.log(`
+  く__,.ヘヽ.        /  ,ー､ 〉
+           ＼ ', !-─‐-i  /  /´
+           ／｀ｰ'       L/／｀ヽ､
+         /   ／,   /|   ,   ,       ',
+       ｲ   / /-‐/  ｉ  L_ ﾊ ヽ!   i
+        ﾚ ﾍ 7ｲ｀ﾄ   ﾚ'ｧ-ﾄ､!ハ|   |
+          !,/7 '0'     ´0iソ|    |
+          |.从"    _     ,,,, / |./    |
+          ﾚ'| i＞.､,,__  _,.イ /   .i   |
+            ﾚ'| | / k_７_/ﾚ'ヽ,  ﾊ.  |
+              | |/i 〈|/   i  ,.ﾍ |  i  |
+             .|/ /  ｉ：    ﾍ!    ＼  |
+              kヽ>､ﾊ    _,.ﾍ､    /､!
+              !'〈//｀Ｔ´', ＼ ｀'7'ｰr'
+              ﾚ'ヽL__|___i,___,ンﾚ|ノ
+                  ﾄ-,/  |___./
+                  'ｰ'    !_,.:
+`);
